98f4bbec00a4280a9614f6c9416fc854ad06cf27
Usuario <Usuario@CPD01-M11>
2024-10-17 16:01:13 -0300

    17/10

--- MauiAppGuiaCurso/MainPage.xaml.cs
+++ MauiAppGuiaCurso/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
@@ -12,17 +12,38 @@
        
         private void primeiro_ano_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                Navigation.PushAsync(new Categorias.Primeiro_ano());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
 
         private void segundo_ano_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                Navigation.PushAsync(new Categorias.Segundo_ano());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
 
         private void terceiro_ano_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                Navigation.PushAsync(new Categorias.Terceiro_ano());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
