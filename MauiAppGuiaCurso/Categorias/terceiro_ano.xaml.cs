cdd42ec92b87050ba3c475b83076c7175b01b8af
Usuario <Usuario@CPD02-M07>
2024-10-22 12:27:24 -0300

    22/10

--- MauiAppGuiaCurso/Categorias/terceiro_ano.xaml.cs
+++ MauiAppGuiaCurso/Categorias/terceiro_ano.xaml.cs
@@ -9,31 +9,73 @@ public partial class Terceiro_ano : ContentPage
 
     private void btnProgramacaoweb3_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            Navigation.PushAsync(new Materias.Programacaoweb3());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnMobile3_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            Navigation.PushAsync(new Materias.Mobile3());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnInternet_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            Navigation.PushAsync(new Materias.Internet());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnTcc_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            Navigation.PushAsync(new Materias.Tcc());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnQualidade_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            Navigation.PushAsync(new Materias.Software());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnSistemas_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            Navigation.PushAsync(new Materias.Sistemasembarcados());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
